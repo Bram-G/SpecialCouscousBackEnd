baec0a670de770a8d4a0ce73fba6d86f1673fda3
Bram Gibson <Bram.gibson@hotmail.com>
2026-03-17 16:21:10 -0700

    quick cleanup after import fail

---
+++ cleanup.js
@@ -0,0 +1,4 @@
+﻿const { sequelize } = require('./src/models');
+sequelize.query(DELETE FROM "GroupMembers" WHERE "GroupId" = 1 AND "UserId" IN (SELECT id FROM "Users" WHERE username IN ('BramTestUser','Sydtest','Ellietest','Timtest','Kyletest','Austintest')))
+  .then(() => { console.log('Done!'); process.exit(0); })
+  .catch(e => { console.error(e.message); process.exit(1); });
